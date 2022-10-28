a392bcc74bde502c9c709e92e3a38e6023435304
Caique Bispo <caiquebispo0023@gmail.com>
2022-10-28 10:50:05 -0300

    addicionando name do Model ao Prefixo da URL

--- routes/api/category.php
+++ routes/api/category.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\api\CategoryController;
 
-Route::post('storageCategory', [CategoryController::class, 'storage']);
-Route::put('updateCategory', [CategoryController::class, 'updateCategory']);
-Route::delete('deleteCategory', [CategoryController::class, 'deleteCategory']);
-Route::get('getAllCategory', [CategoryController::class, 'getAllCategory']);
+Route::post('category/storageCategory', [CategoryController::class, 'storage']);
+Route::put('category/updateCategory', [CategoryController::class, 'updateCategory']);
+Route::delete('category/deleteCategory', [CategoryController::class, 'deleteCategory']);
+Route::get('category/getAllCategory', [CategoryController::class, 'getAllCategory']);
--- routes/api/users.php
+++ routes/api/users.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\api\UserController;
 
-Route::post('storageUser', [UserController::class, 'storageUser']);
-Route::get('getUser', [UserController::class, 'getUser']);
-Route::get('getAllUsers', [UserController::class, 'getAllUsers']);
-Route::put('updateUser', [UserController::class, 'updateUser']);
-Route::delete('deleteUser', [UserController::class, 'deleteUser']);
+Route::post('user/storageUser', [UserController::class, 'storageUser']);
+Route::get('user/getUser', [UserController::class, 'getUser']);
+Route::get('user/getAllUsers', [UserController::class, 'getAllUsers']);
+Route::put('user/updateUser', [UserController::class, 'updateUser']);
+Route::delete('user/deleteUser', [UserController::class, 'deleteUser']);
